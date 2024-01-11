a304a6de199f7ef3a099117482fd88c28680e4ff
Todd Sparkman <toddsparkman@yahoo.com>
2024-01-11 17:47:39 -0600

    revert to express

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const path = require('path');
 
 var https = require('https');
 var http = require('http');
+const exp = require('constants');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Redirect all routes to the 'index.html' file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
-http.createServer(app).listen(80);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
